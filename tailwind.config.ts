65c13152f30d12945a85d63d72135822dace3f18
rfaaelnc <rfaaelnc@gmail.com>
2024-05-03 10:46:43 -0300

    chore(tailwind) adjustments

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import daisyui from "daisyui";
+import daisyui from 'daisyui';
 
 export default {
   plugins: [daisyui],
   daisyui: { themes: [], logs: false },
-  content: ["./**/*.tsx"],
+  content: ['./**/*.tsx'],
   theme: {
     container: { center: true },
     extend: {
       animation: {
-        sliding: "sliding 30s linear infinite",
+        sliding: 'sliding 30s linear infinite',
       },
       keyframes: {
         sliding: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-50%)" },
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
         },
+      },
+      backgroundImage: {
+        'detalhe-1':
+          "url('//ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/8604/dc02980d-641f-4b5c-bb44-92c4111df3b1')",
+        'detalhe-2':
+          "url('//ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/8604/8dafbffa-642e-40f3-b12b-acbfc8a4396a')",
+        'detalhe-3':
+          "url('//ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/8604/504f8212-c580-4e52-9635-314007d47cd8')",
       },
     },
   },
