96bd26200fc8a4f2d5e6326d6d1ef38ab4fede4a
rfaaelnc <rfaaelnc@gmail.com>
2024-05-03 08:42:55 -0300

    feat(budget): added section

---
+++ sections/Budget.tsx
@@ -0,0 +1,29 @@
+export interface Props {
+  title?: string;
+  button?: {
+    label: string;
+    link: string;
+  }
+  image?: {
+    src?: ImageWidget;
+    alt?: string;
+  };
+}
+
+
+export default function Budget({
+  title = "GOSTARIA DE FAZER UM ORÇAMENTO?",
+  button = {
+    label: "ENTRAR EM CONTATO",
+    link: "#"
+  },
+  image = {
+    src: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/8604/f079d539-accd-455e-a465-f698574bda1c",
+    alt: ""
+  } }: Props) {
+  return (
+    <div>
+      text
+    </div>
+  )
+}
