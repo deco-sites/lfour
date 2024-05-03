7422b0767756686c32183abdeab2f064b13609b6
rfaaelnc <rfaaelnc@gmail.com>
2024-05-03 11:11:44 -0300

    feat: added new structure on (header, hero)

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,6 +1,7 @@
-import type { ImageWidget } from "apps/admin/widgets.ts";
-import Image from "apps/website/components/Image.tsx";
-import Icon from "../components/ui/Icon.tsx";
+import Image from 'apps/website/components/Image.tsx';
+import type { ImageWidget } from 'apps/admin/widgets.ts';
+
+type Type = 'dark' | 'light';
 
 export interface CTA {
   id?: string;
@@ -23,120 +24,133 @@ export interface Nav {
   };
 }
 
+export const ColorType: Record<Type, string> = {
+  dark: '[#E3D6C5]',
+  light: 'base-100',
+};
+
+export const StyleType: Record<'background' | 'color', string> = {
+  background: 'bg-',
+  color: 'text-',
+};
+
+const generateLineStyles = (position: string) => `
+  absolute ${position} z-50 block h-0.5 w-7 bg-[#E3D6C5] transition-all duration-200 ease-out 
+`;
+
+const lineStyles = [
+  generateLineStyles('top-[-0.7rem]') +
+  'peer-checked:translate-y-[0.7rem] peer-checked:rotate-[45deg]',
+  generateLineStyles('top-[-0.35rem]') + 'peer-checked:opacity-0',
+  generateLineStyles('top-[0]') +
+  'peer-checked:-translate-y-[0.2rem] peer-checked:-rotate-[45deg]',
+];
+
 export default function Header({
   logo = {
-    src:
-      "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/67120bcd-936a-4ea5-a760-02ed5c4a3d04",
-    alt: "Logo",
+    src: 'https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/67120bcd-936a-4ea5-a760-02ed5c4a3d04',
+    alt: 'Logo',
   },
   navigation = {
     links: [
-      { label: "Home", url: "/" },
-      { label: "About us", url: "/" },
-      { label: "Princing", url: "/" },
-      { label: "Contact", url: "/" },
+      { label: 'Home', url: '/' },
+      { label: 'About us', url: '/' },
+      { label: 'Princing', url: '/' },
+      { label: 'Contact', url: '/' },
     ],
     buttons: [
-      { id: "change-me-1", href: "/", text: "Change me", outline: false },
-      { id: "change-me-2", href: "/", text: "Change me", outline: true },
+      { id: 'change-me-1', href: '/', text: 'Change me', outline: false },
+      { id: 'change-me-2', href: '/', text: 'Change me', outline: true },
     ],
   },
 }: Nav) {
   return (
-    <nav class="drawer drawer-end">
-      <input id="mobile-drawer-nav" type="checkbox" class="drawer-toggle" />
-
-      {/* main content */}
-      <div class="drawer-content container lg:px-0 px-4 flex gap-8 items-center justify-between py-4">
-        <a href="/">
-          <Image src={logo.src || ""} width={100} height={28} alt={logo.alt} />
-        </a>
-
-        <div class="hidden items-center justify-between lg:flex w-full">
-          <ul class="flex">
-            {navigation.links.map((link) => (
-              <li>
-                <a
-                  href={link.url}
-                  aria-label={link.label}
-                  class="link no-underline hover:underline p-4"
-                >
-                  {link.label}
-                </a>
-              </li>
+    <header class="bg-[#201209] relative z-50">
+      <nav class="container mx-auto lg:px-0 px-4">
+        <div class="flex gap-8 items-center justify-between py-4 lg:py-8 grid grid-cols-3">
+          <label
+            class="cursor-pointer lg:hidden pt-6 relative z-40"
+            for="menu-mobile"
+          >
+            <input class="hidden peer" type="checkbox" id="menu-mobile" />
+            {lineStyles.map((style, index) => (
+              <div key={index} class={`relative ${style}`}></div>
             ))}
+            <div class="backdrop-blur-sm bg-black/50 fixed h-full hidden inset-0 peer-checked:block w-full z-40">
+              &nbsp;
+            </div>
+            <div class="duration-500 fixed h-full overflow-y-auto overscroll-y-none peer-checked:translate-x-0 right-0 top-0 transition translate-x-full w-full z-40">
+              <div class="bg-base-100 flex flex-col float-right gap-8 min-h-full pt-12 px-6 shadow-2xl w-1/2">
+                <ul class="flex flex-col gap-8">
+                  {navigation?.links.map((link) => (
+                    <li>
+                      <a href={link.url} aria-label={link.label}>
+                        {link.label}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+                <ul class="flex items-center gap-3">
+                  {navigation.buttons?.map((item) => (
+                    <a
+                      key={item?.id}
+                      id={item?.id}
+                      href={item?.href}
+                      target={item?.href.includes('http') ? '_blank' : '_self'}
+                      class={`font-normal btn btn-primary ${item.outline && 'btn-outline'
+                        }`}
+                    >
+                      {item?.text}
+                    </a>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </label>
+
+          <ul class="hidden items-center justify-between lg:flex w-full">
+            <ul class="flex">
+              {navigation.links.map((link) => (
+                <li>
+                  <a
+                    href={link.url}
+                    aria-label={link.label}
+                    class="link no-underline hover:underline p-4 text-[#E3D6C5]"
+                  >
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </ul>
-          <ul class="flex gap-3">
+
+          <div class=" flex justify-center">
+            <a class="absolute left-0 right-0 top-2 mx-auto w-max" href="/">
+              <Image
+                src={logo.src || ''}
+                width={280}
+                height={141}
+                alt={logo.alt}
+              />
+            </a>
+          </div>
+
+          <ul class="flex justify-end gap-3">
             {navigation.buttons?.map((item) => (
               <a
                 key={item?.id}
                 id={item?.id}
-                href={item?.href ?? "#"}
-                target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
-                  item.outline && "btn-outline"
-                }`}
+                href={item?.href}
+                target={item?.href.includes('http') ? '_blank' : '_self'}
+                class={`font-normal btn btn-primary ${item.outline && 'btn-outline'
+                  } text-[#251B0E] bg-[#E3D6C5] hover:opacity-90 hover:text-[#251B0E] hover:bg-[#E3D6C5]`}
               >
                 {item?.text}
               </a>
             ))}
           </ul>
         </div>
-
-        <label
-          htmlFor="mobile-drawer-nav"
-          class="flex lg:hidden btn btn-ghost drawer-button"
-        >
-          <Icon id="Bars3" size={24} strokeWidth={0.1} />
-        </label>
-      </div>
-
-      {/* sidebar */}
-      <aside class="drawer-side z-50">
-        {/* Close when clicking on overlay */}
-        <label
-          htmlFor="mobile-drawer-nav"
-          aria-label="close sidebar"
-          class="drawer-overlay"
-        />
-
-        <div class="flex flex-col gap-8 min-h-full w-80 bg-base-100 text-base-content">
-          <a class="p-4" href="/">
-            <Image
-              src={logo.src || ""}
-              width={100}
-              height={28}
-              alt={logo.alt}
-            />
-          </a>
-
-          <ul class="menu">
-            {navigation?.links.map((link) => (
-              <li>
-                <a href={link.url} aria-label={link.label}>
-                  {link.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-
-          <ul class="p-4 flex items-center gap-3">
-            {navigation.buttons?.map((item) => (
-              <a
-                key={item?.id}
-                id={item?.id}
-                href={item?.href ?? "#"}
-                target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
-                  item.outline && "btn-outline"
-                }`}
-              >
-                {item?.text}
-              </a>
-            ))}
-          </ul>
-        </div>
-      </aside>
-    </nav>
+      </nav>
+    </header>
   );
 }
--- sections/HeroBanner.tsx
+++ sections/HeroBanner.tsx
@@ -44,11 +44,11 @@ const DEFAULT_PROPS = {
     {
       content: {
         description:
-          "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
+          "A qualidade na fabricação são fundamentais para garantir a satisfação do cliente",
         avatar:
-          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/7286de42-e9c5-4fcb-ae8b-b992eea4b78e",
+          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/8604/1af34311-6f5e-4d07-b596-13bac1876637",
         alt: "Avatar",
-        name: "Name Surname",
+        name: "Sob Medida",
         position: "Position, Company name",
       },
     },
@@ -111,19 +111,30 @@ function SliderItem(
       id={id}
       class="relative overflow-y-hidden w-full min-h-[292px]"
     >
-      <div class="flex flex-col justify-center gap-16 p-8 border border-[#E3D6C5] text-[#E3D6C5] rounded-2xl h-full">
-        <p class="text-lg text-center">{content?.description}</p>
-        <div class="flex items-center justify-center gap-5">
+      <div class="flex flex-col justify-center text-[#E3D6C5] h-full">
+        <div class="flex flex-col items-center justify-center">
           <Image
-            class="object-cover w-14 h-14 rounded-full"
-            alt={content?.alt}
+            width={1920}
+            class="w-full object-fit"
+            sizes="(max-width: 640px) 100vw, 30vw"
             src={content?.avatar || ""}
-            width={56}
-            height={56}
+            alt={content?.alt}
+            decoding="async"
+            loading="lazy"
           />
-          <div class="flex flex-col">
+
+          <div class="relative container">
+            <div class="flex bg-[#201209] px-9 pt-4 pb-5 absolute left-0 bottom-24" style="transform: skew(-30deg)">
+              <div class="container" style="transform: skew(30deg)">
+                <h2 class="text-[#E3D6C5]">{content?.name}</h2>
+                <p class="text-[#DCC9AB] font-light">{content?.description}</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-col hidden">
             <p class="font-semibold text-[#E3D6C5]">{content?.name}</p>
             <p class="text-[#E3D6C5]">{content?.position}</p>
+            <p class="text-lg text-center">{content?.description}</p>
           </div>
         </div>
       </div>
@@ -145,7 +156,7 @@ function Dots({ slides, interval = 0 }: Props) {
           `,
         }}
       />
-      <ul class="carousel col-span-full gap-3 z-10">
+      <ul class="carousel col-span-full z-10">
         {slides?.map((_, index) => (
           <li class="carousel-item">
             <Slider.Dot index={index}>
@@ -208,14 +219,9 @@ function Carousel(props: Props) {
       id={id}
       class="bg-[#251C0D]"
     >
-      <div class="bg-detalhe-3 bg-repeat-x h-[11px] relative -top-[10px]"></div>
-
-      <div class="min-h-min flex flex-col lg:container md:max-w-6xl lg:mx-auto mx-4 py-12 lg:py-28">
-        <h2 class="text-4xl text-[#E3D6C5] leading-snug pb-12 lg:pb-16 text-center">
-          {title}
-        </h2>
+      <div class="min-h-min flex flex-col">
         <Slider
-          class="carousel carousel-center w-full col-span-full row-span-full gap-6"
+          class="carousel carousel-center w-full"
           rootId={id}
           interval={interval && interval * 1e3}
           infinite
@@ -233,14 +239,13 @@ function Carousel(props: Props) {
           ))}
         </Slider>
 
-        <div class="flex justify-between pt-8 lg:px-16">
-          {props.dots && <Dots slides={slides} interval={interval} />}{" "}
-          {props.arrows && <Buttons />}
+        <div class="relative container">
+          <div class="flex justify-between pt-8 lg:px-16 absolute">
+            {props.dots && <Dots slides={slides} interval={interval} />}{" "}
+            {props.arrows && <Buttons />}
+          </div>
         </div>
-
       </div>
-
-      <div class="bg-detalhe-3 bg-repeat-x h-[11px] relative -bottom-[10px] rotate-180"></div>
     </div>
   );
 }
